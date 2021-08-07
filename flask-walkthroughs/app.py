34283701e5a4a4f249b3a28bf6c3e7573469095a
Mohammed Zayan <zayanit@gmail.com>
2021-08-07 21:34:19 +0300

    Raise auth errors
    Lesson 4:
    Access and Authorization
    4. Using RBAC in Flask

--- flask-walkthroughs/app.py
+++ flask-walkthroughs/app.py
@@ -101,12 +101,19 @@ def verify_decode_jwt(token):
                 'code': 'invalid_header',
                 'description': 'Unable to find the appropriate key.'
             }, 400)
+            
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
-        abort(400)
+        raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Permissions not included in JWT.'
+            }, 400)
 
     if permission not in payload['permissions']:
-        abort(403)
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
 
     return True
 
